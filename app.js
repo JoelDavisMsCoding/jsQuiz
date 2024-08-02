9f206b4de05f71f218752a2fa83701c0fd74298c
JoelDavisMsCoding <166549396+JoelDavisMsCoding@users.noreply.github.com>
2024-08-02 12:00:34 -0500

    Submitting Test

---
+++ app.js
@@ -0,0 +1,26 @@
+let fruit = ["apple", "banana", "cherry",];
+console.log(fruit[1]);
+//---------------------------------------------
+let numbers = [1, 2, 3, 4,];
+sum = 0;
+sum += numbers[0]; + numbers[2];
+console.log(`${numbers[0]} + ${numbers[2]} = ${sum}`);
+//--------------------------------------------------------
+let languages = ["JavaScript", "Python", "Java",];
+languages[1] = "Ruby";
+console.log(languages);
+//--------------------------------------------------------
+let age = 20;
+if (age >= 18 ){console.log("Elgible to vote");}
+else {console.log("Not elgible to vote.");}
+//---------------------------------------------------------
+let cities = ["New York", "Los Angeles", "Chicago"];
+let lastCity = cities[2];
+console.log(lastCity);
+//---------------------------------------------------------
+/* Write a line of code that declares a variable x and assigns it the value 7. Then,
+use an if-else statement to check if x is greater than 5 or less than 10, and log "In
+range" if true, and "Out of range" otherwise. */
+let x = 7
+if (x > 5 || x < 10) {console.log("In range");}
+else{console.log("Out of range");}
